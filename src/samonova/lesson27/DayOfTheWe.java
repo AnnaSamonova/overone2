ad3a369981ffbe4b86b1ee33197b4e79683e2d52
AnnaSamonova <vannilinka@mail.ru>
2021-12-28 19:46:54 +0300

    add new file

---
+++ src/samonova/lesson27/DayOfTheWe.java
@@ -0,0 +1,12 @@
+package samonova.lesson27;
+
+public enum DayOfTheWe {
+
+    MONDAY,
+    TUESDAY,
+    WENESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
---
+++ src/samonova/lesson27/Lesson27.java
@@ -0,0 +1,9 @@
+package samonova.lesson27;
+
+public class Lesson27 {
+    public static void main(String[] args) {
+       DayOfTheWe day = DayOfTheWe.MONDAY;
+
+        System.out.println(day);
+    }
+}
